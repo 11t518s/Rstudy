9b641ba7e19b1ea02a3e31649a4ac6d26101deb0
11t518s <11t518s20161759@gmail.com>
2021-09-30 23:54:41 +0900

    chapter3

---
+++ homeWork chapter3.R
@@ -0,0 +1,13 @@
+# 2 R에서 제공하는 CO2 데이터 셋을 대상으로 파일을 저장하시오
+
+Co2Data <- CO2
+
+# Treatment 칼럼 값이 nonchiled인 경우 CO2_df1.csv파일로 행번호를 제외하고 저장한다.
+
+library(data.table)
+
+CO2_df1 = subset(Co2Data, Treatment=="nonchilled")
+CO2_df1
+write.csv(CO2_df1,"CO2_df1.csv", row.names=F, quote=F)
+
+setwd()

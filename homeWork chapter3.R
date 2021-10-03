c792acf9b124ef601d441cc406f24922e7a4e3af
11t518s <11t518s20161759@gmail.com>
2021-10-03 13:11:42 +0900

    uploaded homework

--- homeWork chapter3.R
+++ homeWork chapter3.R
@@ -4,10 +4,10 @@ Co2Data <- CO2
 
 # Treatment 칼럼 값이 nonchiled인 경우 CO2_df1.csv파일로 행번호를 제외하고 저장한다.
 
-library(data.table)
-
 CO2_df1 = subset(Co2Data, Treatment=="nonchilled")
-CO2_df1
 write.csv(CO2_df1,"CO2_df1.csv", row.names=F, quote=F)
 
-setwd()
+# Treatment 칼럼 값이 chiled인 경우 CO2_df2.csv파일로 행번호를 제외하고 저장한다.
+
+CO2_df2 = subset(Co2Data, Treatment=="chilled")
+write.csv(CO2_df1,"CO2_df2.csv", row.names=F, quote=F)

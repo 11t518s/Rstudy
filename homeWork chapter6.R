67b6d43a86981e77495976e9a6edf26088d9c84f
11t518s <11t518s20161759@gmail.com>
2021-10-14 01:09:14 +0900

    오탈자 수정

--- homeWork chapter6.R
+++ homeWork chapter6.R
@@ -9,7 +9,7 @@ iris
 filter(iris, Petal.Length>=1.5)
 
   # 파이프 연산자 활용
-  iris %>% filter(., Petal.Length>=1.5)
+  iris %>% filter(Petal.Length>=1.5)
 
 
 # 6-2 1의 결과에서 1,3,5번 칼럼을 선택하시오
@@ -41,7 +41,8 @@ summarise(groupedSelectedAndFilterdIrisIncludediffColumn, meanOfSepalLength=mean
 
 # 6-5 reshape2 패키지를 이용하여 단계별로 iris 데이터 셋을 처리하시오
 # 6-5-1 꽃의 종류(Species)를 기준으로 '넓은 형식'을 '긴 형식'으로 변경하기
-
+install.packages("reshape2")
+library(reshape2)
 longIris = melt(iris, id="Species")
 longIris
   
@@ -55,3 +56,19 @@ sumofIrisbySpecies
 
   # 파이프 연산자 활용
   iris %>% melt(id="Species") %>% dcast(Species ~ variable, value.var = "value", sum)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
